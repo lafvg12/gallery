4f191817b336c7baba2751cb7607fa052919659b
Andrés Fernández <lafvg12@hotmail.com>
2022-04-06 01:00:38 -0500

    add variables for S3

--- src/image/service/image.service.ts
+++ src/image/service/image.service.ts
@@ -3,16 +3,37 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Image } from '../entities/image.entity';
 import { Model } from 'mongoose';
 import { UpdateImageDto, CreateImageDto } from '../dtos/image.dto';
+import { AwsConfig } from '../../config';
+
 import AwsClient from '../libs/index';
 
 @Injectable()
 export class ImageService {
-  constructor(@InjectModel(Image.name) private imageModel: Model<Image>) {}
+  REGION: string;
+  bucket: string;
+  s3Client;
+
+  constructor(
+    @InjectModel(Image.name) private imageModel: Model<Image>,
+    @Inject('AWS_CONFIG')
+    private readonly awsConfig: AwsConfig,
+  ) {
+    this.REGION = awsConfig.region;
+    this.bucket = awsConfig.bucket;
+    this.s3Client = {
+      region: this.REGION,
+      credentials: {
+        accessKeyId: awsConfig.accessKeyId,
+        secretAccessKey: awsConfig.secretAccessKey,
+      },
+    };
+  }
 
   async getAllImages() {
     const images = this.imageModel.find().exec();
     return images;
   }
+
   async findOne(id: string) {
     const product = await this.imageModel.findById(id).exec();
     if (!product) {
@@ -20,10 +41,12 @@ export class ImageService {
     }
     return product;
   }
+
   create(payload: CreateImageDto) {
     const image = new this.imageModel(payload);
     return image.save();
   }
+
   update(id: string, changes: UpdateImageDto) {
     const product = this.imageModel
       .findByIdAndUpdate(id, { $set: changes }, { new: true })
@@ -39,35 +62,26 @@ export class ImageService {
   }
 
   async sendAws(file: Express.Multer.File) {
-    const REGION = 'us-east-1'; //e.g. "us-east-1"
+    const REGION = this.REGION; //e.g. "us-east-1"
     const KEY = file.originalname;
-    // Create an Amazon S3 service client object.
 
-    const client = {
-      region: REGION,
-      credentials: {
-        accessKeyId: 'XXXXXX',
-        secretAccessKey: 'XXXXXXX',
-      },
-    };
-    // const s3Client = new S3Client(client);
     const params = {
-      Bucket: 'gallery1989', // The name of the bucket. For example, 'sample_bucket_101'.
-      Key: KEY, // The name of the object. For example, 'sample_upload.txt'.
-      Body: file.buffer, // The content of the object. For example, 'Hello world!".
+      Bucket: this.bucket,
+      Key: KEY,
+      Body: file.buffer,
     };
 
     const param1 = {
-      Bucket: 'gallery1989',
+      Bucket: this.bucket,
       Key: KEY,
     };
     const awsClient = new AwsClient();
 
-    const other = awsClient.sendFile(client, params).then((results) => {
+    const other = awsClient.sendFile(this.s3Client, params).then((results) => {
       return results.$metadata.httpStatusCode;
     });
-    const urlG = awsClient.getUrl(param1, client);
-
+    const urlImage = awsClient.getUrl(param1, this.s3Client);
+    console.log(urlImage);
     return other;
   }
 }
